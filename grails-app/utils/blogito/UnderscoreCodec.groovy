082fcb4bafcc6c322d57e1407da132749eb09bfe
Jorge Mario <jorgemario@thepublicgroup.com>
2012-09-21 09:19:54 -0500

    added spring security plugin

--- grails-app/utils/blogito/UnderscoreCodec.groovy
+++ grails-app/utils/blogito/UnderscoreCodec.groovy
@@ -2,11 +2,11 @@ package blogito
 
 class UnderscoreCodec {
 
-	public static encode = { target ->
-		return target.replaceAll(" ", "_")
-	}
+    public static encode = { target ->
+        return target.replaceAll(" ", "_")
+    }
 	
-	static decode = { target ->
-		return target.replaceAll("_", " ")
-	}
+    static decode = { target ->
+        return target.replaceAll("_", " ")
+    }
 }
